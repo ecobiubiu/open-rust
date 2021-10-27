godot-rust/gdnative

Minimal async-await foundations
This sets the foundations for async-await support in godot-rust, based on the
original idea in #284. However, although the tests work, this is not a full
implementation:

- Async methods can only be registered manually using `build_method`. Macro
  syntax and implementation are out of the scope of this PR.
- The runtime types aren't registered automatically yet.
  Users need to manually call `register_runtime` and `terminate_runtime`
  functions in their library lifecycle hooks. Improving this is out of the
  scope of this PR for now.
- The crate is currently re-exported as `gdnative::asn`, instead of the much
  longer `async_yield`. The name is open to discussion -- I don't like it very
  much.
- Only local spawners are supported, due to issues with thread safety. Users
  may off-load tasks that don't contain `yield`-likes to thread pool spawners
  using something like `futures::future::Remote`, however.
- Panics in async methods don't currently behave very well. Their
  `FunctionState`-likes simply block forever and any outstanding bridge
  objects for futures can be leaked.

gdnative-async/src/method.rs
@@ -1,0 +1,126 @@
+use std::future::Future;
+use std::marker::PhantomData;
+use std::sync::Arc;
+
+use futures_task::{LocalFutureObj, LocalSpawn, SpawnError};
+
+use gdnative_core::core_types::{ToVariant, Variant};
+use gdnative_core::log::{self, Site};
+use gdnative_core::nativescript::init::{Method, Varargs};
+use gdnative_core::nativescript::{NativeClass, RefInstance};
+use gdnative_core::thread_access::Shared;
+
+use crate::rt::Context;
+
+/// Trait for async methods. When exported, such methods return `FunctionState`-like
+/// objects that can be manually resumed or yielded to completion.
+///
+/// Async methods are always spawned locally on the thread where they were created,
+/// and never sent to another thread. This is so that we can ensure the safety of
+/// emitting signals from the `FunctionState`-like object. If you need to off-load
+/// some task to another thread, consider using something like
+/// `futures::future::Remote` to spawn it remotely on a thread pool.
+pub trait AsyncMethod<C: NativeClass>: Send + Sync + 'static {
+    /// Spawns the future for result of this method with `spawner`. This is done so
+    /// that implementors of this trait do not have to name their future types.
+    ///
+    /// If the `spawner` object is not used, the method call will fail, output an error,
+    /// and return a `Nil` variant.
+    fn spawn_with(&self, spawner: Spawner<'_, C>);
+
+    /// Returns an optional site where this method is defined. Used for logging errors in FFI wrappers.
+    ///
+    /// Default implementation returns `None`.
+    #[inline]
+    fn site() -> Option<Site<'static>> {
+        None
+    }
+}
+
+pub struct Spawner<'a, C: NativeClass> {
+    sp: &'static dyn LocalSpawn,
+    ctx: Context,
+    this: RefInstance<'a, C, Shared>,
+    args: Varargs<'a>,
+    result: &'a mut Option<Result<(), SpawnError>>,
+    /// Remove Send and Sync
+    _marker: PhantomData<*const ()>,
+}
+
+impl<'a, C: NativeClass> Spawner<'a, C> {
+    /// Consumes this `Spawner` and spawns a future returned by the closure. This indirection
+    /// is necessary so that implementors of the `AsyncMethod` trait do not have to name their
+    /// future types.
+    pub fn spawn<F, R>(self, f: F)
+    where
+        F: FnOnce(Arc<Context>, RefInstance<'_, C, Shared>, Varargs<'_>) -> R,
+        R: Future<Output = Variant> + 'static,
+    {
+        let ctx = Arc::new(self.ctx);
+        let future = f(Arc::clone(&ctx), self.this, self.args);
+        *self.result = Some(
+            self.sp
+                .spawn_local_obj(LocalFutureObj::new(Box::new(async move {
+                    let value = future.await;
+                    ctx.resolve(value);
+                }))),
+        );
+    }
+}
+
+/// Adapter for async methods that implements `Method` and can be registered.
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Async<F> {
+    f: F,
+}
+
+impl<F> Async<F> {
+    /// Wrap `f` in an adapter that implements `Method`.
+    #[inline]
+    pub fn new(f: F) -> Self {
+        Async { f }
+    }
+}
+
+impl<C: NativeClass, F: AsyncMethod<C>> Method<C> for Async<F> {
+    fn call(&self, this: RefInstance<'_, C, Shared>, args: Varargs<'_>) -> Variant {
+        if let Some(sp) = crate::executor::local_spawn() {
+            let ctx = Context::new();
+            let func_state = ctx.func_state();
+
+            let mut result = None;
+            self.f.spawn_with(Spawner {
+                sp,
+                ctx,
+                this,
+                args,
+                result: &mut result,
+                _marker: PhantomData,
+            });
+
+            match result {
+                Some(Ok(())) => func_state.to_variant(),
+                Some(Err(err)) => {
+                    log::error(
+                        Self::site().unwrap_or_default(),
+                        format_args!("unable to spawn future: {}", err),
+                    );
+                    Variant::new()
+                }
+                None => {
+                    log::error(
+                        Self::site().unwrap_or_default(),
+                        format_args!("implementation did not spawn a future"),
+                    );
+                    Variant::new()
+                }
+            }
+        } else {
+            log::error(
+                Self::site().unwrap_or_default(),
+                "a global executor must be set before any async methods can be called on this thread",
+            );
+            Variant::new()
+        }
+    }
+}

gdnative-async/src/rt.rs
@@ -1,0 +1,128 @@
+use std::marker::PhantomData;
+
+use gdnative_bindings::Object;
+use gdnative_core::object::SubClass;
+use once_cell::sync::OnceCell;
+use thiserror::Error;
+
+use gdnative_core::core_types::{GodotError, Variant};
+use gdnative_core::nativescript::{InitHandle, Instance, RefInstance};
+use gdnative_core::thread_access::Shared;
+use gdnative_core::TRef;
+
+use crate::future;
+
+mod bridge;
+mod func_state;
+
+use func_state::FuncState;
+
+static REGISTRATION: OnceCell<()> = OnceCell::new();
+
+#[derive(Debug, Error)]
+#[error("async runtime must only be initialized once")]
+pub struct InitError {
+    _private: (),
+}
+
+impl InitError {
+    fn new() -> Self {
+        InitError { _private: () }
+    }
+}
+
+/// Context for creating `yield`-like futures in async methods.
+pub struct Context {
+    func_state: Instance<FuncState, Shared>,
+    /// Remove Send and Sync
+    _marker: PhantomData<*const ()>,
+}
+
+impl Context {
+    pub(crate) fn new() -> Self {
+        Context {
+            func_state: FuncState::new().into_shared(),
+            _marker: PhantomData,
+        }
+    }
+
+    pub(crate) fn func_state(&self) -> Instance<FuncState, Shared> {
+        self.func_state.clone()
+    }
+
+    fn safe_func_state(&self) -> RefInstance<'_, FuncState, Shared> {
+        // SAFETY: FuncState objects are bound to their origin threads in Rust, and
+        // Context is !Send, so this is safe to call within this type.
+        // Non-Rust code is expected to be following the official guidelines as per
+        // the global safety assumptions. Since a reference of `FuncState` is held by
+        // Rust, it voids the assumption to send the reference to any thread aside from
+        // the one where it's created.
+        unsafe { self.func_state.assume_safe() }
+    }
+
+    pub(crate) fn resolve(&self, value: Variant) {
+        func_state::resolve(self.safe_func_state(), value);
+    }
+
+    /// Returns a future that waits until the corresponding `FunctionState` object
+    /// is manually resumed from GDScript, and yields the argument to `resume` or `Nil`
+    /// if nothing is passed.
+    ///
+    /// Calling this function will put the associated `FunctionState`-like object in
+    /// resumable state, and will make it emit a `resumable` signal if it isn't in that
+    /// state already.
+    ///
+    /// Only the most recent future created from this `Context` is guaranteed to resolve
+    /// upon a `resume` call. If any previous futures weren't `await`ed to completion, they
+    /// are no longer guaranteed to resolve, and have unspecified, but safe behavior
+    /// when polled.
+    pub fn until_resume(&self) -> future::Yield<Variant> {
+        let (future, resume) = future::make();
+        func_state::make_resumable(self.safe_func_state(), resume);
+        future
+    }
+
+    /// Returns a future that waits until the specified signal is emitted, if connection succeeds.
+    /// Yields any arguments emitted with the signal.
+    ///
+    /// Only the most recent future created from this `Context` is guaranteed to resolve
+    /// when the signal is emitted. If any previous futures weren't `await`ed to completion, they
+    /// are no longer guaranteed to resolve, and have unspecified, but safe behavior
+    /// when polled.
+    ///
+    /// # Errors
+    ///
+    /// If connection to the signal failed.
+    pub fn signal<C>(
+        &self,
+        obj: TRef<'_, C>,
+        signal: &str,
+    ) -> Result<future::Yield<Vec<Variant>>, GodotError>
+    where
+        C: SubClass<Object>,
+    {
+        let (future, resume) = future::make();
+        bridge::SignalBridge::connect(obj.upcast(), signal, resume)?;
+        Ok(future)
+    }
+}
+
+pub fn register_runtime(handle: &InitHandle) -> Result<(), InitError> {
+    let mut called = false;
+
+    REGISTRATION.get_or_init(|| {
+        handle.add_class::<bridge::SignalBridge>();
+        handle.add_class::<func_state::FuncState>();
+        called = true;
+    });
+
+    if called {
+        Ok(())
+    } else {
+        Err(InitError::new())
+    }
+}
+
+pub fn terminate_runtime() {
+    bridge::terminate();
+}

ecobiubiu/open-rust

gdnative-async/src/method.rs
@@ -1,0 +1,126 @@
+use std::future::Future;
+use std::marker::PhantomData;
+use std::sync::Arc;
+
+use futures_task::{LocalFutureObj, LocalSpawn, SpawnError};
+
+use gdnative_core::core_types::{ToVariant, Variant};
+use gdnative_core::log::{self, Site};
+use gdnative_core::nativescript::export::{Method, Varargs};
+use gdnative_core::nativescript::{NativeClass, RefInstance};
+use gdnative_core::object::ownership::Shared;
+
+use crate::rt::Context;
+
+/// Trait for async methods. When exported, such methods return `FunctionState`-like
+/// objects that can be manually resumed or yielded to completion.
+///
+/// Async methods are always spawned locally on the thread where they were created,
+/// and never sent to another thread. This is so that we can ensure the safety of
+/// emitting signals from the `FunctionState`-like object. If you need to off-load
+/// some task to another thread, consider using something like
+/// `futures::future::Remote` to spawn it remotely on a thread pool.
+pub trait AsyncMethod<C: NativeClass>: Send + Sync + 'static {
+    /// Spawns the future for result of this method with `spawner`. This is done so
+    /// that implementors of this trait do not have to name their future types.
+    ///
+    /// If the `spawner` object is not used, the method call will fail, output an error,
+    /// and return a `Nil` variant.
+    fn spawn_with(&self, spawner: Spawner<'_, C>);
+
+    /// Returns an optional site where this method is defined. Used for logging errors in FFI wrappers.
+    ///
+    /// Default implementation returns `None`.
+    #[inline]
+    fn site() -> Option<Site<'static>> {
+        None
+    }
+}
+
+pub struct Spawner<'a, C: NativeClass> {
+    sp: &'static dyn LocalSpawn,
+    ctx: Context,
+    this: RefInstance<'a, C, Shared>,
+    args: Varargs<'a>,
+    result: &'a mut Option<Result<(), SpawnError>>,
+    /// Remove Send and Sync
+    _marker: PhantomData<*const ()>,
+}
+
+impl<'a, C: NativeClass> Spawner<'a, C> {
+    /// Consumes this `Spawner` and spawns a future returned by the closure. This indirection
+    /// is necessary so that implementors of the `AsyncMethod` trait do not have to name their
+    /// future types.
+    pub fn spawn<F, R>(self, f: F)
+    where
+        F: FnOnce(Arc<Context>, RefInstance<'_, C, Shared>, Varargs<'_>) -> R,
+        R: Future<Output = Variant> + 'static,
+    {
+        let ctx = Arc::new(self.ctx);
+        let future = f(Arc::clone(&ctx), self.this, self.args);
+        *self.result = Some(
+            self.sp
+                .spawn_local_obj(LocalFutureObj::new(Box::new(async move {
+                    let value = future.await;
+                    ctx.resolve(value);
+                }))),
+        );
+    }
+}
+
+/// Adapter for async methods that implements `Method` and can be registered.
+#[derive(Clone, Copy, Default, Debug)]
+pub struct Async<F> {
+    f: F,
+}
+
+impl<F> Async<F> {
+    /// Wrap `f` in an adapter that implements `Method`.
+    #[inline]
+    pub fn new(f: F) -> Self {
+        Async { f }
+    }
+}
+
+impl<C: NativeClass, F: AsyncMethod<C>> Method<C> for Async<F> {
+    fn call(&self, this: RefInstance<'_, C, Shared>, args: Varargs<'_>) -> Variant {
+        if let Some(sp) = crate::executor::local_spawn() {
+            let ctx = Context::new();
+            let func_state = ctx.func_state();
+
+            let mut result = None;
+            self.f.spawn_with(Spawner {
+                sp,
+                ctx,
+                this,
+                args,
+                result: &mut result,
+                _marker: PhantomData,
+            });
+
+            match result {
+                Some(Ok(())) => func_state.to_variant(),
+                Some(Err(err)) => {
+                    log::error(
+                        Self::site().unwrap_or_default(),
+                        format_args!("unable to spawn future: {}", err),
+                    );
+                    Variant::new()
+                }
+                None => {
+                    log::error(
+                        Self::site().unwrap_or_default(),
+                        format_args!("implementation did not spawn a future"),
+                    );
+                    Variant::new()
+                }
+            }
+        } else {
+            log::error(
+                Self::site().unwrap_or_default(),
+                "a global executor must be set before any async methods can be called on this thread",
+            );
+            Variant::new()
+        }
+    }
+}

gdnative-async/src/rt.rs
@@ -1,0 +1,129 @@
+use std::marker::PhantomData;
+
+use gdnative_bindings::Object;
+use gdnative_core::object::SubClass;
+use once_cell::sync::OnceCell;
+use thiserror::Error;
+
+use gdnative_core::core_types::{GodotError, Variant};
+use gdnative_core::nativescript::export::InitHandle;
+use gdnative_core::nativescript::{Instance, RefInstance};
+use gdnative_core::object::ownership::Shared;
+use gdnative_core::object::TRef;
+
+use crate::future;
+
+mod bridge;
+mod func_state;
+
+use func_state::FuncState;
+
+static REGISTRATION: OnceCell<()> = OnceCell::new();
+
+#[derive(Debug, Error)]
+#[error("async runtime must only be initialized once")]
+pub struct InitError {
+    _private: (),
+}
+
+impl InitError {
+    fn new() -> Self {
+        InitError { _private: () }
+    }
+}
+
+/// Context for creating `yield`-like futures in async methods.
+pub struct Context {
+    func_state: Instance<FuncState, Shared>,
+    /// Remove Send and Sync
+    _marker: PhantomData<*const ()>,
+}
+
+impl Context {
+    pub(crate) fn new() -> Self {
+        Context {
+            func_state: FuncState::new().into_shared(),
+            _marker: PhantomData,
+        }
+    }
+
+    pub(crate) fn func_state(&self) -> Instance<FuncState, Shared> {
+        self.func_state.clone()
+    }
+
+    fn safe_func_state(&self) -> RefInstance<'_, FuncState, Shared> {
+        // SAFETY: FuncState objects are bound to their origin threads in Rust, and
+        // Context is !Send, so this is safe to call within this type.
+        // Non-Rust code is expected to be following the official guidelines as per
+        // the global safety assumptions. Since a reference of `FuncState` is held by
+        // Rust, it voids the assumption to send the reference to any thread aside from
+        // the one where it's created.
+        unsafe { self.func_state.assume_safe() }
+    }
+
+    pub(crate) fn resolve(&self, value: Variant) {
+        func_state::resolve(self.safe_func_state(), value);
+    }
+
+    /// Returns a future that waits until the corresponding `FunctionState` object
+    /// is manually resumed from GDScript, and yields the argument to `resume` or `Nil`
+    /// if nothing is passed.
+    ///
+    /// Calling this function will put the associated `FunctionState`-like object in
+    /// resumable state, and will make it emit a `resumable` signal if it isn't in that
+    /// state already.
+    ///
+    /// Only the most recent future created from this `Context` is guaranteed to resolve
+    /// upon a `resume` call. If any previous futures weren't `await`ed to completion, they
+    /// are no longer guaranteed to resolve, and have unspecified, but safe behavior
+    /// when polled.
+    pub fn until_resume(&self) -> future::Yield<Variant> {
+        let (future, resume) = future::make();
+        func_state::make_resumable(self.safe_func_state(), resume);
+        future
+    }
+
+    /// Returns a future that waits until the specified signal is emitted, if connection succeeds.
+    /// Yields any arguments emitted with the signal.
+    ///
+    /// Only the most recent future created from this `Context` is guaranteed to resolve
+    /// when the signal is emitted. If any previous futures weren't `await`ed to completion, they
+    /// are no longer guaranteed to resolve, and have unspecified, but safe behavior
+    /// when polled.
+    ///
+    /// # Errors
+    ///
+    /// If connection to the signal failed.
+    pub fn signal<C>(
+        &self,
+        obj: TRef<'_, C>,
+        signal: &str,
+    ) -> Result<future::Yield<Vec<Variant>>, GodotError>
+    where
+        C: SubClass<Object>,
+    {
+        let (future, resume) = future::make();
+        bridge::SignalBridge::connect(obj.upcast(), signal, resume)?;
+        Ok(future)
+    }
+}
+
+pub fn register_runtime(handle: &InitHandle) -> Result<(), InitError> {
+    let mut called = false;
+
+    REGISTRATION.get_or_init(|| {
+        handle.add_class::<bridge::SignalBridge>();
+        handle.add_class::<func_state::FuncState>();
+        called = true;
+    });
+
+    if called {
+        Ok(())
+    } else {
+        Err(InitError::new())
+    }
+}
+
+pub fn terminate_runtime() {
+    bridge::terminate();
+}