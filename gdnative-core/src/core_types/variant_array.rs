chitoyuu/godot-rust

Remove or change APIs causing UB
* Dictionary
  * get_next() -- removed; wasn't possible to use safely (last element = UB).
    No need for this method, given Iterator impl
* VariantArray
  * get(), get_ref(), get_mut_ref() --
    introduced bounds check
  * clear(), remove(), erase(), resize(), push(), pop(), push_front(), pop_front(), insert() --
    removed; should be done explicitly via assume_unique()
* Variant
  * call() -- can execute any code, is now unsafe like Object::call()

Comment-out now unused proc-macro 'doc_variant_collection_safety'

gdnative-core/src/core_types/variant_array.rs
@@ -1,8 +1,6 @@
 use std::iter::{Extend, FromIterator};
 use std::marker::PhantomData;
 
-use gdnative_impl_proc_macros::doc_variant_collection_safety;
-
 use crate::private::get_api;
 use crate::sys;
 
@@ -50,6 +48,7 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     /// Returns a copy of the element at the given offset.
     #[inline]
     pub fn get(&self, idx: i32) -> Variant {
+        self.check_bounds(idx);
         unsafe { Variant((get_api().godot_array_get)(self.sys(), idx)) }
     }
 
@@ -63,6 +62,7 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     /// `Variant` is reference-counted and thus cheaply cloned. Consider using `get` instead.
     #[inline]
     pub unsafe fn get_ref(&self, idx: i32) -> &Variant {
+        self.check_bounds(idx);
         Variant::cast_ref((get_api().godot_array_operator_index_const)(
             self.sys(),
             idx,
@@ -79,6 +79,7 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     #[inline]
     #[allow(clippy::mut_from_ref)]
     pub unsafe fn get_mut_ref(&self, idx: i32) -> &mut Variant {
+        self.check_bounds(idx);
         Variant::cast_mut_ref((get_api().godot_array_operator_index)(self.sys_mut(), idx))
     }
 
@@ -220,6 +221,15 @@ impl<Access: ThreadAccess> VariantArray<Access> {
         std::mem::forget(self);
         VariantArray::from_sys(sys)
     }
+
+    fn check_bounds(&self, idx: i32) {
+        assert!(
+            idx >= 0 && idx < self.len(),
+            "Index {} out of bounds (len {})",
+            idx,
+            self.len()
+        );
+    }
 }
 
 /// Operations allowed on Dictionaries that can only be referenced to from the current thread.
@@ -334,78 +344,6 @@ impl VariantArray<Shared> {
     #[inline]
     pub fn new_shared() -> Self {
         VariantArray::<Unique>::new().into_shared()
-    }
-
-    /// Clears the array, resizing to 0.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn clear(&self) {
-        (get_api().godot_array_clear)(self.sys_mut());
-    }
-
-    /// Removes the element at `idx`.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn remove(&self, idx: i32) {
-        (get_api().godot_array_remove)(self.sys_mut(), idx)
-    }
-
-    /// Removed the first occurrence of `val`.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn erase<T: ToVariant>(&self, val: T) {
-        (get_api().godot_array_erase)(self.sys_mut(), val.to_variant().sys())
-    }
-
-    /// Resizes the array, filling with `Nil` if necessary.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn resize(&self, size: i32) {
-        (get_api().godot_array_resize)(self.sys_mut(), size)
-    }
-
-    /// Appends an element at the end of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn push<T: OwnedToVariant>(&self, val: T) {
-        (get_api().godot_array_push_back)(self.sys_mut(), val.owned_to_variant().sys());
-    }
-
-    /// Removes an element at the end of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn pop(&self) -> Variant {
-        Variant((get_api().godot_array_pop_back)(self.sys_mut()))
-    }
-
-    /// Appends an element to the front of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn push_front<T: OwnedToVariant>(&self, val: T) {
-        (get_api().godot_array_push_front)(self.sys_mut(), val.owned_to_variant().sys());
-    }
-
-    /// Removes an element at the front of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn pop_front(&self) -> Variant {
-        Variant((get_api().godot_array_pop_front)(self.sys_mut()))
-    }
-
-    /// Insert a new int at a given position in the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn insert<T: OwnedToVariant>(&self, at: i32, val: T) {
-        (get_api().godot_array_insert)(self.sys_mut(), at, val.owned_to_variant().sys())
     }
 }
 
@@ -675,12 +613,22 @@ godot_test!(test_array {
 
 godot_test!(
     test_array_debug {
+        use std::panic::catch_unwind;
+
         let arr = VariantArray::new(); // []
         arr.push(&Variant::from_str("hello world"));
         arr.push(&Variant::from_bool(true));
         arr.push(&Variant::from_i64(42));
 
         assert_eq!(format!("{:?}", arr), "[GodotString(hello world), Bool(True), I64(42)]");
+
+        let panic_get = catch_unwind(|| { arr.get(3); });
+        let panic_get_ref = catch_unwind(|| { unsafe { arr.get_ref(3) }; });
+        let panic_get_mut_ref = catch_unwind(|| { unsafe { arr.get_mut_ref(3) }; });
+
+        assert!(panic_get.is_err(), "get() out of bounds causes panic");
+        assert!(panic_get_ref.is_err(), "get_mut() out of bounds causes panic");
+        assert!(panic_get_mut_ref.is_err(), "get_mut_ref() out of bounds causes panic");
     }
 );
 

ecobiubiu/open-rust

  * set(), get(), get_ref(), get_mut_ref() -- introduced bounds check

gdnative-core/src/core_types/variant_array.rs
@@ -1,8 +1,6 @@
 use std::iter::{Extend, FromIterator};
 use std::marker::PhantomData;
 
-use gdnative_impl_proc_macros::doc_variant_collection_safety;
-
 use crate::private::get_api;
 use crate::sys;
 
@@ -44,12 +42,14 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     /// Sets the value of the element at the given offset.
     #[inline]
     pub fn set<T: OwnedToVariant>(&self, idx: i32, val: T) {
+        self.check_bounds(idx);
         unsafe { (get_api().godot_array_set)(self.sys_mut(), idx, val.owned_to_variant().sys()) }
     }
 
     /// Returns a copy of the element at the given offset.
     #[inline]
     pub fn get(&self, idx: i32) -> Variant {
+        self.check_bounds(idx);
         unsafe { Variant((get_api().godot_array_get)(self.sys(), idx)) }
     }
 
@@ -63,6 +63,7 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     /// `Variant` is reference-counted and thus cheaply cloned. Consider using `get` instead.
     #[inline]
     pub unsafe fn get_ref(&self, idx: i32) -> &Variant {
+        self.check_bounds(idx);
         Variant::cast_ref((get_api().godot_array_operator_index_const)(
             self.sys(),
             idx,
@@ -79,6 +80,7 @@ impl<Access: ThreadAccess> VariantArray<Access> {
     #[inline]
     #[allow(clippy::mut_from_ref)]
     pub unsafe fn get_mut_ref(&self, idx: i32) -> &mut Variant {
+        self.check_bounds(idx);
         Variant::cast_mut_ref((get_api().godot_array_operator_index)(self.sys_mut(), idx))
     }
 
@@ -220,6 +222,15 @@ impl<Access: ThreadAccess> VariantArray<Access> {
         std::mem::forget(self);
         VariantArray::from_sys(sys)
     }
+
+    fn check_bounds(&self, idx: i32) {
+        assert!(
+            idx >= 0 && idx < self.len(),
+            "Index {} out of bounds (len {})",
+            idx,
+            self.len()
+        );
+    }
 }
 
 /// Operations allowed on Dictionaries that can only be referenced to from the current thread.
@@ -334,78 +345,6 @@ impl VariantArray<Shared> {
     #[inline]
     pub fn new_shared() -> Self {
         VariantArray::<Unique>::new().into_shared()
-    }
-
-    /// Clears the array, resizing to 0.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn clear(&self) {
-        (get_api().godot_array_clear)(self.sys_mut());
-    }
-
-    /// Removes the element at `idx`.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn remove(&self, idx: i32) {
-        (get_api().godot_array_remove)(self.sys_mut(), idx)
-    }
-
-    /// Removed the first occurrence of `val`.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn erase<T: ToVariant>(&self, val: T) {
-        (get_api().godot_array_erase)(self.sys_mut(), val.to_variant().sys())
-    }
-
-    /// Resizes the array, filling with `Nil` if necessary.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn resize(&self, size: i32) {
-        (get_api().godot_array_resize)(self.sys_mut(), size)
-    }
-
-    /// Appends an element at the end of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn push<T: OwnedToVariant>(&self, val: T) {
-        (get_api().godot_array_push_back)(self.sys_mut(), val.owned_to_variant().sys());
-    }
-
-    /// Removes an element at the end of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn pop(&self) -> Variant {
-        Variant((get_api().godot_array_pop_back)(self.sys_mut()))
-    }
-
-    /// Appends an element to the front of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn push_front<T: OwnedToVariant>(&self, val: T) {
-        (get_api().godot_array_push_front)(self.sys_mut(), val.owned_to_variant().sys());
-    }
-
-    /// Removes an element at the front of the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn pop_front(&self) -> Variant {
-        Variant((get_api().godot_array_pop_front)(self.sys_mut()))
-    }
-
-    /// Insert a new int at a given position in the array.
-    ///
-    #[doc_variant_collection_safety]
-    #[inline]
-    pub unsafe fn insert<T: OwnedToVariant>(&self, at: i32, val: T) {
-        (get_api().godot_array_insert)(self.sys_mut(), at, val.owned_to_variant().sys())
     }
 }
 
@@ -675,12 +614,24 @@ godot_test!(test_array {
 
 godot_test!(
     test_array_debug {
+        use std::panic::catch_unwind;
+
         let arr = VariantArray::new(); // []
         arr.push(&Variant::from_str("hello world"));
         arr.push(&Variant::from_bool(true));
         arr.push(&Variant::from_i64(42));
 
         assert_eq!(format!("{:?}", arr), "[GodotString(hello world), Bool(True), I64(42)]");
+
+        let set = catch_unwind(|| { arr.set(3, 7i64); });
+        let get = catch_unwind(|| { arr.get(3); });
+        let get_ref = catch_unwind(|| { unsafe { arr.get_ref(3) }; });
+        let get_mut_ref = catch_unwind(|| { unsafe { arr.get_mut_ref(3) }; });
+
+        assert!(set.is_err(), "set() out of bounds causes panic");
+        assert!(get.is_err(), "get() out of bounds causes panic");
+        assert!(get_ref.is_err(), "get_mut() out of bounds causes panic");
+        assert!(get_mut_ref.is_err(), "get_mut_ref() out of bounds causes panic");
     }
 );
 